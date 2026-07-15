b33d6e962a7dfe3984d32133245567405d06d77b
Antony Revellin Clerc <antonyrevellin@gmail.com>
2026-07-15 17:35:03 +0200

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "meal-organizer-hotfix-v5";
+const CACHE_NAME = "meal-organizer-v6";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=5",
-  "./app.js?v=5",
-  "./config.js?v=5",
+  "./styles.css?v=6",
+  "./app.js?v=6",
+  "./config.js?v=6",
   "./manifest.webmanifest",
   "./assets/icon.svg",
   "./assets/icon-192.png",
