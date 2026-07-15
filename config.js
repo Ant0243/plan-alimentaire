6fa014821e11074089db04c7142e6b10ba8ef68b
Antony Revellin Clerc <antonyrevellin@gmail.com>
2026-07-15 16:29:08 +0200

    Update Supabase configuration values

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  supabaseUrl: "",
-  supabasePublishableKey: ""
+  supabaseUrl: "https://nhwsobfcldetiredlnee.supabase.co",
+  supabasePublishableKey: "sb_publishable_4V2Wr3LKgJ6iL1VjYS1tMA_8vUFv-vc"
 };
